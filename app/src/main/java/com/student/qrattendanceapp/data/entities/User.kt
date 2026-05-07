f5ff1d2df964f3b44eb2e0dbc1284aa81d919956
Rostonpinto <rostonp4@gmail.com>
2026-05-08 01:24:25 +0530

    feat: create room database entity models for user , classes , sessions and attendance

--- app/src/main/java/com/student/qrattendanceapp/data/entities/User.kt
+++ app/src/main/java/com/student/qrattendanceapp/data/entities/User.kt
@@ -1,2 +1,13 @@
 package com.student.qrattendanceapp.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val email: String,
+    val password: String,
+    val role: String
+)
