501f4f80680e27afad04a0aeedbe010317e9b1b9
Rostonpinto <rostonp4@gmail.com>
2026-05-08 01:26:12 +0530

    feat: add DAO interface for database operations and attendace management

--- app/src/main/java/com/student/qrattendanceapp/data/entities/ClassRoom.kt
+++ app/src/main/java/com/student/qrattendanceapp/data/entities/ClassRoom.kt
@@ -1,2 +1,13 @@
 package com.student.qrattendanceapp.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "classes")
+data class ClassRoom(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val subject: String,
+    val teacherId: Int,
+    val classCode: String
+)
